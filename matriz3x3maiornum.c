10c5c11d976824562003048d25eaa4f1d4a73bb1
Mariana <57501545+wari99@users.noreply.github.com>
2022-11-07 20:55:57 -0300

    Create matriz3x3maiornum.c

---
+++ matriz3x3maiornum.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int i;
+    int j;
+    int maior;
+    int matriz[3][3]; 
+    
+    for(i= 0; i < 3; i++){
+        for(j= 0; j < 3; j++){
+            printf("Elemento[%d][%d] = ", i,j);
+            scanf("%d", &matriz[i][j]);
+        }
+    } 
+    
+    maior = matriz[i][j];
+
+    for(i= 0; i < 3; i++){
+        for(j= 0; j < 3; j++){
+            if(matriz[i][j] > maior){
+                maior = matriz[i][j];
+            }
+        }
+    }  
+    
+    printf("O maior valor da matriz e %d", maior);
+    
+    return 0;
+}
