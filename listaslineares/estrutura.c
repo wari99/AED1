c4d47f31ab9040ed966d5bfd8400f5a86ef50ff1
Mariana <57501545+wari99@users.noreply.github.com>
2022-11-18 18:05:46 -0300

    Create estrutura.c

---
+++ listaslineares/estrutura.c
@@ -0,0 +1,15 @@
+/* max representa a quantidade maxima de elementos que a lista pode armazenar
+n representa o numero de elementos na lista.*/
+
+#include <stdio.h>
+#define max 6
+    typedef struct listaSeq{
+        int valores[max];
+        int n;
+    }listaSeq;
+
+int main(){
+    listaSeq lista; 
+    
+    return 0;
+}
